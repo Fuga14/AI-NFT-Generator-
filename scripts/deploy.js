fc11734f83ae2e3000dd90dd087b8cbd529392c3
Fuga14 <vladyslavpereder@gmail.com>
2023-03-16 22:02:22 +0200

    minting function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,20 @@
-const hre = require('hardhat');
+const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'AI NFT Generator';
-  const SYMBOL = 'AINFT';
-  const COST = hre.ethers.utils.parseUnits('1', 'ether'); // 1 ETH
+  const NAME = "AI Generated NFT";
+  const SYMBOL = "AINFT";
+  const COST = hre.ethers.utils.parseUnits("1", "ether"); // 1 ETH
 
-  const NFT = await hre.ethers.getContractFactory('NFT');
+  const NFT = await hre.ethers.getContractFactory("NFT");
   const nft = await NFT.deploy(NAME, SYMBOL, COST);
   await nft.deployed();
 
   console.log(`Deployed NFT Contract at: ${nft.address}`);
 }
 
-main().catch(error => {
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
